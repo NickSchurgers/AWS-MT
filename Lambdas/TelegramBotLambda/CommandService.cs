f3e289b213c7c21c2ad6181e56c4e63a88eedd23
Nick Schürgers <n.schurgers@outlook.com>
2021-11-01 09:55:12 +0100

    Add project files.

---
+++ Lambdas/TelegramBotLambda/CommandService.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.Lambda.Core;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBotLambda
+{
+    public class CommandService
+    {
+        private readonly TelegramBotClient _botClient;
+
+        public CommandService()
+        {
+            _botClient = new TelegramBotClient("2086559527:AAFnxRQKqtNBrRX6hULsHGygMcWr8F2aurg");
+        }
+
+
+        public Task SendAsync(Update update)
+        {
+            LambdaLogger.Log(update.Message.Text);
+            return _botClient.SendTextMessageAsync(update.Message.Chat.Id, update.Message.Text);
+        }
+    }
+}
---
+++ Lambdas/TelegramBotLambda/Function.cs
@@ -0,0 +1,71 @@
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+// This project specifies the serializer used to convert Lambda event into .NET classes in the project's main 
+// main function. This assembly register a serializer for use when the project is being debugged using the
+// AWS .NET Mock Lambda Test Tool.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace TelegramBotLambda
+{
+    public class Function
+    {
+        private static CommandService _commandService;
+        /// <summary>
+        /// The main entry point for the custom runtime.
+        /// </summary>
+        /// <param name="args"></param>
+        private static async Task Main(string[] args)
+        {
+            try
+            {
+                _commandService = new CommandService();
+                Func<APIGatewayHttpApiV2ProxyRequest, ILambdaContext, Task<APIGatewayHttpApiV2ProxyResponse>> func = FunctionHandler;
+                using (var handlerWrapper = HandlerWrapper.GetHandlerWrapper(func, new DefaultLambdaJsonSerializer()))
+                using (var bootstrap = new LambdaBootstrap(handlerWrapper))
+                {
+                    await bootstrap.RunAsync();
+                }
+            }
+            catch(Exception e)
+            {
+                LambdaLogger.Log(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        ///
+        /// To use this handler to respond to an AWS event, reference the appropriate package from 
+        /// https://github.com/aws/aws-lambda-dotnet#events
+        /// and change the string input parameter to the desired event type.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest input, ILambdaContext context)
+        {
+            try
+            {
+                LambdaLogger.Log(input.Body);
+                var updateEvent = Newtonsoft.Json.JsonConvert.DeserializeObject<Update>(input.Body);
+                await _commandService.SendAsync(updateEvent);
+
+                return new APIGatewayHttpApiV2ProxyResponse { StatusCode = (int)HttpStatusCode.NoContent };
+            }
+            catch (Exception e)
+            {
+                LambdaLogger.Log(e.Message);
+                LambdaLogger.Log(e.StackTrace);
+
+                return new APIGatewayHttpApiV2ProxyResponse { StatusCode = (int)HttpStatusCode.InternalServerError };
+            }
+        }
+    }
+}
