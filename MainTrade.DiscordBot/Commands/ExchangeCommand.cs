6833776b52765986aa7629648bc5d0a2eb631fa9
Nick Schürgers <n.schurgers@outlook.com>
2021-11-19 12:51:36 +0100

    Added working C# discordbot PoC

---
+++ MainTrade.DiscordBot/Commands/ExchangeCommand.cs
@@ -0,0 +1,18 @@
+﻿using DSharpPlus;
+using DSharpPlus.SlashCommands;
+using System;
+using System.Threading.Tasks;
+
+namespace Maintrade.DiscordBot.Commands
+{
+    public class ExchangeCommand : ApplicationCommandModule
+    {
+        [SlashCommand("exchange", "Retrieve a list of supported exchanges")]
+        public async Task Command(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+
+            await ctx.EditResponseAsync(new DSharpPlus.Entities.DiscordWebhookBuilder().WithContent($"I deferred this message, bro!{Environment.NewLine}This C# lib seems to be working without bs. Just need the redo the ec2 lol."));
+        }
+    }
+}
---
+++ MainTrade.DiscordBot/Program.cs
@@ -0,0 +1,29 @@
+﻿using DSharpPlus;
+using DSharpPlus.SlashCommands;
+using Maintrade.DiscordBot.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace Maintrade.DiscordBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN"),
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            var commands = discord.UseSlashCommands();
+
+            commands.RegisterCommands<ExchangeCommand>();
+            commands.RegisterCommands<PortfolioCommand>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
